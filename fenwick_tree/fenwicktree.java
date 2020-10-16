7f4fa34c40607a2077216176dba36b5ecd1dca7c
yash handa <yashhanda500@gmail.com>
2020-10-16 16:37:34 +0530

    update

--- fenwick_tree/fenwicktree.java
+++ fenwick_tree/fenwicktree.java
@@ -1,9 +1,12 @@
-public class fenwicktree{  //BIT(Binary Indexed Tree)
+public static class fenwicktree{  //BIT(Binary Indexed Tree)
     public static int[]BIT;
-   public static int sum(int[]arr,int i){
-       return get_sum(arr,i);
+    fenwicktree(int n){
+        BIT=new int[n+1];
+    }
+   public static int sum(int i){
+       return get_sum(i);
    }
-   public static int get_sum(int[]arr,int i){
+   public static int get_sum(int i){
        int index=i+1;
        int sum=0;
        while(index>0){
@@ -13,9 +16,15 @@ public class fenwicktree{  //BIT(Binary Indexed Tree)
        return sum;
    }
    public static void update(int[]arr,int value,int i){
+       int diff=value-arr[i];
+       arr[i]=value;
+       int n=arr.length;
+    update_diff(diff,i);
+   }
+   public static void update_diff(int value,int i){
        int index=i+1;
-        int n=arr.length;
-       while(index<=n){
+       int n=BIT.length;
+       while(index<n){
            BIT[index]+=value;
            index+=index&(-index);
        }
@@ -23,21 +32,11 @@ public class fenwicktree{  //BIT(Binary Indexed Tree)
    public static void construct(int[]arr){
        int n=arr.length;
        for(int i=0;i<n;i++)
-       update(arr,arr[i],i);
+       update_diff(arr[i],i);
    }
    public static void display(){
     for(int i=0;i<BIT.length;i++)
     System.out.print(BIT[i]+" ");
     System.out.println();
 }
-    public static void main(String[]args){
-        int[]arr={1,3,5,7,9,11};
-        int n=arr.length;
-        BIT=new int[n+1];
-        construct(arr);
-        display();
-        int s=sum(arr,4);
-        System.out.println(s);
-        
-    }
 }
